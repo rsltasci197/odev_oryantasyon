21c9feb2909ba5a8a17b13f41a9140bf6986b166
resul taşçı <147501138+rsltasci197@users.noreply.github.com>
2024-02-19 15:24:09 +0300

    Update ArkadasSayi.java

--- ArkadasSayi.java
+++ ArkadasSayi.java
@@ -1,35 +1,31 @@
-package com.company;
-
 import java.util.Scanner;
 
 public class ArkadasSayi {
-    public static void main(String args[]){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Birinci Sayıyı Giriniz: ");
         int sayi1 = scanner.nextInt();
         System.out.print("İkinci Sayıyı Giriniz: ");
         int sayi2 = scanner.nextInt();
-        if(Arkadasmi(sayi1,sayi2)){
+
+        if (arkadasSayiMi(sayi1, sayi2)) {
             System.out.println("Bu iki sayı arkadaştır");
-        }else{
+        } else {
             System.out.println("Bu iki sayı arkadaş değildir.");
         }
     }
-    public static boolean Arkadasmi(int sayi1, int sayi2){
-        if(sayi1 == BolenToplami(sayi2) && sayi2 == BolenToplami(sayi1)){
-            return true;
-        }else{
-            return false;
-        }
+
+    public static boolean arkadasSayiMi(int sayi1, int sayi2) {
+        return (bolenToplami(sayi1) == sayi2) && (bolenToplami(sayi2) == sayi1);
     }
 
-    public static int BolenToplami(int sayi){
-        int bolentoplami = 0;
-        for(int i = 1; i < sayi; i++){
-            if(sayi%i == 0){
-                bolentoplami = bolentoplami+i;
+    public static int bolenToplami(int sayi) {
+        int toplam = 0;
+        for (int i = 1; i <= sayi / 2; i++) {
+            if (sayi % i == 0) {
+                toplam += i;
             }
         }
-        return bolentoplami;
+        return toplam;
     }
 }
